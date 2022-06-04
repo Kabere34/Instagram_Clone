ccca41117e52887323d9e8d1701ff63733fed778
Ivy Kabere <kabereivy@gmail.com>
2022-06-05 01:33:09 +0300

    add url paths for profile page and profile_edit page

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('create_post/', views.create_post, name='create_post'),
     path('login/', auth_views.LoginView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(),name='logout'),
-    
+    path('profile/',views.profile,name='profile'),
+    path('edit/profile',views.profile_edit,name='profile_edit'),
+
+
     # path('registration_form/', views.signup, name='signup'),
     # path('upload/', views.upload, name='upload'),
     # path('upload/', views.upload, name='upload'),
