e5d5e860e53f9212e7bea9ac94bde5bc0a3437b9
Ivy Kabere <kabereivy@gmail.com>
2022-06-04 14:42:36 +0300

    create view function to upload post

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import *
+from .forms import *
 # Create your views here.
 def home(request):
-  return render(request, 'main/index.html')
+  images=Post.objects.all()
+  return render(request, 'main/index.html',{'images':images})
+def upload(request):
+  upload=Post()
+  if request.method == 'POST':
+    upload=PostForm(request.POST,request.FILES)
+    if upload.is_valid():
+      upload.save()
+      return redirect('home')
+    else:
+      return HttpResponse('Your form is invalid')
+  else:
+    return render(request,'upload_form.html',{'upload':upload})
+
