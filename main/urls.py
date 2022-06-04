46623b91f34058d4b3aeb33de4794fdc6281f9c2
Ivy Kabere <kabereivy@gmail.com>
2022-06-04 14:53:55 +0300

    create url paths for update,delete and upload functions

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home')
-
+    path('', views.home, name='home'),
+    path('upload/', views.upload, name='upload'),
+    path('update/<int:post_id>', views.update_post, name='upload'),
+    path('delete/', views.upload_post, name='upload'),
 ]
+
