84dadda2f9305b69e276018edf82de23211fdd5e
Ivy Kabere <kabereivy@gmail.com>
2022-06-07 17:41:09 +0300

    fix urls on index page to display default image

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 from django.template import loader
 # Create your views here.
 
+
+
 @login_required(login_url='/accounts/login/')
 def home(request):
   post = Post.objects.all()
